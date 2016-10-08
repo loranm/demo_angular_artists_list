d1aa07e997494bd9b6e7c110933e62e0a392091f
loran <chmoxster@gmail.com>
2016-10-08 12:21:37 +0200

    "Déplacement des actions d'ajout, d'édition et de sauvegarde dans la factory"

--- public/app/js/services/artistsFactory.js
+++ public/app/js/services/artistsFactory.js
@@ -3,23 +3,42 @@
 (function() {
 
     angular.module('musicApp').factory('artistsFactory', function() {
-        return {
+        var service = {};
+        var data =  {
             artists: [{
                 name: 'Group Love',
                 genre: 'Alternative',
-                rating: 4
+                rating: '4'
             }, {
                 name: 'Beatles',
                 genre: 'Rock',
-                rating: 5
+                rating: '5'
             }, {
                 name: 'Nirvana',
                 genre: 'Grunge',
-                rating: 3
+                rating: '3'
             }]
 
 
+        };
+
+        service.get = function(){
+          return data.artists;
+        };
+
+        service.add = function(artist){
+          data.artists.push(artist)
+        };
+
+        service.getArtist = function(id){
+          return data.artists[id]
         }
+
+        service.save = function(id,artist){
+          data.artists[id] = artist
+        };
+
+        return service;
 
     });
 
