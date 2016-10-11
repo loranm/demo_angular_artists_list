335511feede3b2793446d131aec83c40b8b0a074
loran <chmoxster@gmail.com>
2016-10-11 16:18:54 +0200

    correction d'une erreur sur addController.js

--- public/app/js/controllers/addController.js
+++ public/app/js/controllers/addController.js
@@ -2,7 +2,8 @@
   'use strict';
   angular.module('musicApp').controller('addController', ['$scope', '$location', 'artistsFactory', function($scope, $location, artistsFactory) {
     $scope.artist = {};
-    $scope.grades = [1,2,3,4,5];
+    $scope.grades = artistsFactory.getGrades();
+    $scope.genres = artistsFactory.getGenres();
 
     $scope.add = function() {
       artistsFactory.add($scope.artist);
--- public/app/js/module.js
+++ public/app/js/module.js
@@ -2,4 +2,10 @@
   "use strict";
   angular.module('musicApp', ['ngRoute', 'ngResource']);
 
+  angular.module('musicApp').run(['$rootScope', '$templateCache', function($rootScope, $templateCache) {
+   $rootScope.$on('$viewContentLoaded', function() {
+      $templateCache.removeAll();
+   });
+}]);
+
 })();
